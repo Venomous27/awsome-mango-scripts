2d3369f3eb03203834978079b5b776668f2eba7c
Venomous27 <venomousrudra27@gmail.com>
2026-09-14 17:59:27 +0530

    Added scripts folder with wallpaper script

---
+++ scripts/wallpaper-changer-paywal/wallpaper.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+IMG=$(find ~/Pictures/Wallpapers/ -type f | sed "s|$HOME/Pictures/Wallpapers/||" | fzf --layout=reverse)
+
+if [ "$IMG" == "" ]; then exit 1; fi
+
+awww img ~/Pictures/Wallpapers/$IMG --transition-type fade --transition-duration 1
+
+wal -i ~/Pictures/Wallpapers/$IMG
